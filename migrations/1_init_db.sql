aac5f8c65468f75b80c739d4faf1ba2e86c08f19
petrp99 <petr2@bk.ru>
2025-05-23 20:44:29 +0300

    миграция

---
+++ migrations/1_init_db.sql
@@ -0,0 +1,26 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS role
+(
+    id         bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name       text        NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz NOT NULL DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS employee
+(
+    id         bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name       text        NOT NULL,
+    role_id    bigint REFERENCES role (id),
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz NOT NULL DEFAULT now()
+);
+-- +goose StatementEnd
+
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE role;
+DROP TABLE employee;
+-- +goose StatementEnd
