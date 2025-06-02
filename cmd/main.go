6784c17ecbe96df9a228fd53bc8c6ebb8753cf71
petrp99 <petr2@bk.ru>
2025-06-02 17:25:49 +0300

    added service tests

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"fmt"
-	"idm/inner/database"
-	"idm/inner/role"
 )
 
 func main() {
 	fmt.Println("Hello Go")
-	db, _ := database.ConnectDb()
-	service := role.NewService(role.NewRoleRepository(db))
-	save, _ := service.FindAll()
-	fmt.Println(save)
 }
